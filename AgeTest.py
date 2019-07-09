ab9a9aa4ccae4181c36129b0641a8130dc540d0f
Zhenghan Zhang <43033983+darthkenobi5319@users.noreply.github.com>
2019-07-09 14:51:06 -0400

    Initial commit

---
+++ AgeTest.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 16:39:03 2019
+
+@author: admin
+"""
+
+age1 = input('Please input the first age: ')
+
+age2 = input('Please input the second age: ')
+
+avgAge = (int(age1)+int(age2))/2
+
+print('Average age is',avgAge, sep=' ')
---
+++ NameTest.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  8 16:31:19 2019
+
+@author: admin
+"""
+
+name1 = input('Please input the first name: ')
+
+name2 = input('Please input the second name: ')
+
+print(name1,'and',name2,'are sitting next to each other.',sep=' ' )
